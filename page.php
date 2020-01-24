dbee3f70ef584cc2d97d5f0a258f3a780623f635
priyaranjan singh <58334735+priyeranjansingh@users.noreply.github.com>
2020-01-24 03:55:06 -0800

    Create page.php

---
+++ page.php
@@ -0,0 +1,7 @@
+<script>
+var days= new Date(year, month, 0).getDate(); 
+//year in YYYY and month in mm
+
+var days = new Date(2020,02).getDate();
+console.log("number of days in month ",days);
+</scrpt>
